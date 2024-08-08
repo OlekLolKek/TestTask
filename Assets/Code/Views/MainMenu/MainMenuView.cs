a6d0115fe578e1a8a9674b2fa5d37582389912ee
NEDNAR <drakozavrov20@gmail.com>
2024-08-08 19:19:04 +0300

    Added main menu window switching

--- Assets/Code/Views/MainMenu/MainMenuView.cs
+++ Assets/Code/Views/MainMenu/MainMenuView.cs
@@ -8,13 +8,43 @@ namespace Code.Views.MainMenu
         [field: SerializeField] public StartButtonsPanelView ButtonsPanel { get; private set; }
         [field: SerializeField] public NewSimulationPanelView NewSimulationPanel { get; private set; }
 
-        public void ShowButtonsPanel()
+        private void OnEnable()
+        {
+            ButtonsPanel.NewSimulationButtonClick += OnNewSimulationButtonClick;
+            ButtonsPanel.LoadSimulationButtonClick += OnLoadSimulationButtonClick;
+
+            NewSimulationPanel.BackButtonClick += OnNewSimulationBackButtonClick;
+        }
+
+        private void OnDisable()
+        {
+            ButtonsPanel.NewSimulationButtonClick -= OnNewSimulationButtonClick;
+            ButtonsPanel.LoadSimulationButtonClick -= OnLoadSimulationButtonClick;
+
+            NewSimulationPanel.BackButtonClick -= OnNewSimulationBackButtonClick;
+        }
+
+        private void OnNewSimulationButtonClick()
+        {
+            ShowNewSimulationPanel();
+        }
+
+        private void OnLoadSimulationButtonClick()
+        {
+        }
+
+        private void OnNewSimulationBackButtonClick()
+        {
+            ShowButtonsPanel();
+        }
+
+        private void ShowButtonsPanel()
         {
             ButtonsPanel.SetActive(true);
             NewSimulationPanel.SetActive(false);
         }
 
-        public void ShowNewSimulationPanel()
+        private void ShowNewSimulationPanel()
         {
             ButtonsPanel.SetActive(false);
             NewSimulationPanel.SetActive(true);
