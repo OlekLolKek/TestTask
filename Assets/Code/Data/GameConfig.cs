356130a53c6c970a7a34321cf7cad479707a3a19
NEDNAR <drakozavrov20@gmail.com>
2024-08-08 19:16:37 +0300

    Changed config setters

--- Assets/Code/Data/GameConfig.cs
+++ Assets/Code/Data/GameConfig.cs
@@ -9,13 +9,26 @@ namespace Code.Data
         [field: SerializeField] public int FieldSize { get; private set; }
         [field: SerializeField] public int AnimalCount { get; private set; }
         [field: SerializeField] public int AnimalSpeed { get; private set; }
-        
-        
-        public void SetValues(int fieldSize, int animalCount, int animalSpeed)
+
+        public void SetFieldSize(int fieldSize)
         {
             FieldSize = Mathf.Clamp(fieldSize, Constants.MIN_FIELD_SIZE, Constants.MAX_FIELD_SIZE);
+            AnimalCount = Mathf.Clamp(AnimalCount, Constants.MIN_ANIMAL_COUNT, GetMaxAnimalCount());
+        }
+
+        public void SetAnimalCount(int animalCount)
+        {
             AnimalCount = Mathf.Clamp(animalCount, Constants.MIN_ANIMAL_COUNT, FieldSize * FieldSize / 2);
+        }
+
+        public void SetAnimalSpeed(int animalSpeed)
+        {
             AnimalSpeed = Mathf.Clamp(animalSpeed, Constants.MIN_ANIMAL_SPEED, Constants.MAX_ANIMAL_SPEED);
+        }
+
+        public int GetMaxAnimalCount()
+        {
+            return FieldSize * FieldSize / 2;
         }
     }
 }
