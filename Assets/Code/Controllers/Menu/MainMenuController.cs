733829c75ed0ac91198bac79a7928cc764233a89
NEDNAR <drakozavrov20@gmail.com>
2024-08-08 19:20:13 +0300

    Added main menu controllers

---
+++ Assets/Code/Controllers/Menu/MainMenuController.cs
@@ -0,0 +1,25 @@
+﻿using Code.Data;
+using Code.Views.MainMenu;
+using UnityEngine;
+
+
+namespace Code.Controllers.Menu
+{
+    public sealed class MainMenuController : MonoBehaviour
+    {
+        [SerializeField] private GameConfig _config;
+        [SerializeField] private MainMenuView _mainMenu;
+
+        private NewSimulationPanelController _newSimulationPanelController;
+
+        private void Start()
+        {
+            _newSimulationPanelController = new NewSimulationPanelController(_mainMenu.NewSimulationPanel, _config);
+        }
+
+        private void OnDestroy()
+        {
+            _newSimulationPanelController.Dispose();
+        }
+    }
+}
---
+++ Assets/Code/Controllers/Menu/NewSimulationPanelController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Code.Data;
+using Code.Views.MainMenu;
+
+
+namespace Code.Controllers.Menu
+{
+    public sealed class NewSimulationPanelController : IDisposable
+    {
+        private readonly NewSimulationPanelView _view;
+        private readonly GameConfig _config;
+
+        public NewSimulationPanelController(NewSimulationPanelView view, GameConfig config)
+        {
+            _view = view;
+            _config = config;
+
+            _view.FieldSizeSliderValueChanged += OnFieldSizeSliderValueChanged;
+            _view.AnimalCountSliderValueChanged += OnAnimalCountSliderValueChanged;
+            _view.AnimalSpeedSliderValueChanged += OnAnimalSpeedSliderValueChanged;
+        }
+
+        private void OnFieldSizeSliderValueChanged(int value)
+        {
+            _config.SetFieldSize(value);
+            _view.SetAnimalCountSliderMaxValue(_config.GetMaxAnimalCount());
+        }
+
+        private void OnAnimalCountSliderValueChanged(int value)
+        {
+            _config.SetAnimalCount(value);
+        }
+
+        private void OnAnimalSpeedSliderValueChanged(int value)
+        {
+            _config.SetAnimalSpeed(value);
+        }
+
+
+        public void Dispose()
+        {
+            _view.FieldSizeSliderValueChanged -= OnFieldSizeSliderValueChanged;
+            _view.AnimalCountSliderValueChanged -= OnAnimalCountSliderValueChanged;
+            _view.AnimalSpeedSliderValueChanged -= OnAnimalSpeedSliderValueChanged;
+        }
+    }
+}
