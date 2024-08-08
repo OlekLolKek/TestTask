0e17d0e5e2aacfd51855b4e084f527a8b94f5252
NEDNAR <drakozavrov20@gmail.com>
2024-08-08 17:30:40 +0300

    Added configs

---
+++ Assets/Code/Data/Constants.cs
@@ -0,0 +1,13 @@
+﻿namespace Code.Data
+{
+    public static class Constants
+    {
+        public const int MIN_FIELD_SIZE = 2;
+        public const int MAX_FIELD_SIZE = 1000;
+
+        public const int MIN_ANIMAL_SPEED = 1;
+        public const int MAX_ANIMAL_SPEED = 100;
+
+        public const int MIN_ANIMAL_COUNT = 1;
+    }
+}
---
+++ Assets/Code/Data/GameConfig.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+
+namespace Code.Data
+{
+    [CreateAssetMenu(fileName = nameof(GameConfig), menuName = "Config/" + nameof(GameConfig), order = 0)]
+    public sealed class GameConfig : ScriptableObject
+    {
+        [field: SerializeField] public int FieldSize { get; private set; }
+        [field: SerializeField] public int AnimalCount { get; private set; }
+        [field: SerializeField] public int AnimalSpeed { get; private set; }
+        
+        
+        public void SetValues(int fieldSize, int animalCount, int animalSpeed)
+        {
+            FieldSize = Mathf.Clamp(fieldSize, Constants.MIN_FIELD_SIZE, Constants.MAX_FIELD_SIZE);
+            AnimalCount = Mathf.Clamp(animalCount, Constants.MIN_ANIMAL_COUNT, FieldSize * FieldSize / 2);
+            AnimalSpeed = Mathf.Clamp(animalSpeed, Constants.MIN_ANIMAL_SPEED, Constants.MAX_ANIMAL_SPEED);
+        }
+    }
+}
