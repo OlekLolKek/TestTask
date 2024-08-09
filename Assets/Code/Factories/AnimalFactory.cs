bd13dd6d9dfff77506edba8cf038e528fd9ad39d
NEDNAR <drakozavrov20@gmail.com>
2024-08-09 21:26:50 +0300

    Added Animal Factory

---
+++ Assets/Code/Factories/AnimalFactory.cs
@@ -0,0 +1,22 @@
+﻿using Code.Data;
+using Code.Views.Game;
+using UnityEngine;
+
+
+namespace Code.Factories
+{
+    public sealed class AnimalFactory
+    {
+        public AnimalView Create(GameConfig config, GameObject world)
+        {
+            var worldPosition = world.transform.position;
+            var offsetX = Random.Range(-config.FieldSize / 2, config.FieldSize / 2);
+            var offsetZ = Random.Range(-config.FieldSize / 2, config.FieldSize / 2);
+
+            var offset = new Vector3(offsetX, 0.0f, offsetZ);
+            offset.y = world.transform.localScale.y;
+
+            return Object.Instantiate(config.AnimalPrefab, worldPosition + offset, Quaternion.identity);
+        }
+    }
+}
