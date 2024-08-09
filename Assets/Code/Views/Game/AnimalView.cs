44a31cf467959db3c6fbeb764dbd29ab5ec66501
NEDNAR <drakozavrov20@gmail.com>
2024-08-09 21:05:56 +0300

    Added animal view

---
+++ Assets/Code/Views/Game/AnimalView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+
+namespace Code.Views.Game
+{
+    public sealed class AnimalView : BaseView
+    {
+        #region Events
+
+        public event Action<Collider> TriggerEnter;
+
+        #endregion
+
+
+        #region Mono
+
+        private void OnTriggerEnter(Collider other)
+        {
+            TriggerEnter?.Invoke(other);
+        }
+
+        #endregion
+    }
+}
