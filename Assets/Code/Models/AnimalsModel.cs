0e49f4844f25335d7867ac89005c890528daa870
NEDNAR <drakozavrov20@gmail.com>
2024-08-09 21:26:35 +0300

    Added Animal Model

---
+++ Assets/Code/Models/AnimalsModel.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using Code.Data;
+using Code.Factories;
+using Code.Views.Game;
+using UnityEngine;
+
+
+namespace Code.Models
+{
+    public sealed class AnimalsModel
+    {
+        #region Properties
+
+        public HashSet<AnimalView> AnimalViews { get; } = new();
+
+        #endregion
+        
+        
+        #region Fields
+
+        private readonly AnimalFactory _factory;
+        private readonly GameConfig _config;
+
+        #endregion
+        
+
+        #region CodeLife
+
+        public AnimalsModel(GameConfig config)
+        {
+            _config = config;
+
+            _factory = new AnimalFactory();
+        }
+
+        #endregion
+
+
+        #region Methods
+
+        public void InitializeAnimals(GameObject world)
+        {
+            for (var i = 0; i < _config.AnimalCount; ++i)
+            {
+                var animal = _factory.Create(_config, world);
+
+                AnimalViews.Add(animal);
+            }
+        }
+
+        #endregion
+    }
+}
