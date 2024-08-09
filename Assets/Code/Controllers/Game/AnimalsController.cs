debc9ae09f7aee5b4ea42c15b7e2110eba37ad25
NEDNAR <drakozavrov20@gmail.com>
2024-08-09 21:27:03 +0300

    Added animal controller wip

---
+++ Assets/Code/Controllers/Game/AnimalsController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Code.Interfaces.MonoBehaviourCycle;
+using Code.Models;
+using UnityEngine;
+
+
+namespace Code.Controllers.Game
+{
+    public sealed class AnimalsController : ICleanable
+    {
+        #region Fields
+
+        private readonly AnimalsModel _model;
+        private readonly WorldModel _worldModel;
+
+        private bool _initialized = false;
+
+        #endregion
+        
+        
+        #region CodeLife
+
+        public AnimalsController(AnimalsModel model, WorldModel worldModel)
+        {
+            _model = model;
+            _worldModel = worldModel;
+
+            _worldModel.WorldInitialized += OnWorldInitialized;
+        }
+
+        public void Cleanup()
+        {
+            if (!_initialized)
+            {
+                _worldModel.WorldInitialized -= OnWorldInitialized;
+            }
+        }
+
+        #endregion
+
+
+        #region Methods
+
+        private void OnWorldInitialized(GameObject world)
+        {
+            _worldModel.WorldInitialized -= OnWorldInitialized;
+            
+            _model.InitializeAnimals(world);
+
+            _initialized = true;
+        }
+
+        #endregion
+    }
+}
