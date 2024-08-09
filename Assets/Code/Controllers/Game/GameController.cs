9b2f2bfc4ee2eba0ee26ee139500642490c4272a
NEDNAR <drakozavrov20@gmail.com>
2024-08-09 20:13:57 +0300

    Added basic game controller

---
+++ Assets/Code/Controllers/Game/GameController.cs
@@ -0,0 +1,51 @@
+﻿using Code.Data;
+using Code.Models;
+using UnityEngine;
+
+
+namespace Code.Controllers.Game
+{
+    public sealed class GameController : MonoBehaviour
+    {
+        #region Fields
+
+        [SerializeField] private GameConfig _config;
+        [SerializeField] private Camera _camera;
+        
+        private readonly Controllers _controllers = new Controllers();
+
+        #endregion
+
+
+        #region Mono
+
+        private void Start()
+        {
+            var animalsModel = new AnimalsModel(_config);
+
+            _controllers.Start();
+        }
+
+        private void FixedUpdate()
+        {
+            _controllers.FixedUpdate(Time.fixedDeltaTime);
+        }
+
+        private void Update()
+        {
+            _controllers.Update(Time.deltaTime);
+        }
+
+        private void LateUpdate()
+        {
+            _controllers.LateUpdate(Time.deltaTime);
+        }
+
+        private void OnDestroy()
+        {
+            _controllers.Dispose();
+        }
+
+        #endregion
+    }
+}
