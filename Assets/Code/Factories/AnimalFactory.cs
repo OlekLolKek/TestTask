4ac198329d03cdeae89c554b4dd7fc8dc32c17ec
NEDNAR <drakozavrov20@gmail.com>
2024-08-09 21:36:07 +0300

    Added summary for AnimalFactory

--- Assets/Code/Factories/AnimalFactory.cs
+++ Assets/Code/Factories/AnimalFactory.cs
@@ -7,14 +7,19 @@ namespace Code.Factories
 {
     public sealed class AnimalFactory
     {
+        /// <summary>
+        /// Factory method that spawns an Animal specified in the config in a random position in the world.
+        /// </summary>
+        /// <param name="config">The GameConfig with spawn information.</param>
+        /// <param name="world">The World GameObject to spawn the animals on to.</param>
+        /// <returns>The AnimalView of the spawned animal.</returns>
         public AnimalView Create(GameConfig config, GameObject world)
         {
             var worldPosition = world.transform.position;
             var offsetX = Random.Range(-config.FieldSize / 2, config.FieldSize / 2);
             var offsetZ = Random.Range(-config.FieldSize / 2, config.FieldSize / 2);
 
-            var offset = new Vector3(offsetX, 0.0f, offsetZ);
-            offset.y = world.transform.localScale.y;
+            var offset = new Vector3(offsetX, config.AnimalSpawnHeight, offsetZ);
 
             return Object.Instantiate(config.AnimalPrefab, worldPosition + offset, Quaternion.identity);
         }
